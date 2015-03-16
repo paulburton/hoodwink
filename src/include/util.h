b376f2f6ba00bf0e94072c4010ca345ed95795e8
Paul Burton <paul@archlinuxmips.org>
2015-03-16 16:18:21 +0000

    Handle 0 args to VA_NUM_ARGS for C code
    Use preprocessor stringification to handle the case of 0 arguments being
    passed to VA_NUM_ARGS, used for example when outputting syscall debug
    information.
    
    Signed-off-by: Paul Burton <paul@archlinuxmips.org>

--- src/include/util.h
+++ src/include/util.h
@@ -1,8 +1,16 @@
 #ifndef __hoodwink_util_h__
 #define __hoodwink_util_h__
 
-#define VA_NUM_ARGS(...) VA_NUM_ARGS_IMPL(__VA_ARGS__, 8,7,6,5,4,3,2,1)
 #define VA_NUM_ARGS_IMPL(_1,_2,_3,_4,_5,_6,_7,_8,N,...) N
+
+/* note that this won't handle 0 args for assembly! */
+#ifdef __ASSEMBLER__
+# define VA_NUM_ARGS(...) \
+	VA_NUM_ARGS_IMPL(__VA_ARGS__, 8,7,6,5,4,3,2,1)
+#else
+# define VA_NUM_ARGS(...) \
+	(sizeof(#__VA_ARGS__) == sizeof("") ? 0 : VA_NUM_ARGS_IMPL(__VA_ARGS__, 8,7,6,5,4,3,2,1))
+#endif
 
 #define typeof __typeof__
 
