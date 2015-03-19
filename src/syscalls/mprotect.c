5d85721db3d9708c5dc57831cfb363bf778953a4
Paul Burton <paul@archlinuxmips.org>
2015-03-19 00:24:54 +0000

    Translate mprotect flags
    Take advantage of the newly introduced f2b_prot function to translate
    flags passed to mprotect.
    
    Signed-off-by: Paul Burton <paul@archlinuxmips.org>

--- src/syscalls/mprotect.c
+++ src/syscalls/mprotect.c
@@ -1,6 +1,15 @@
+#include "debug.h"
 #include "syscall.h"
 
 uint32_t translate_sys_mprotect(struct sys_state *sys, uint32_t *args)
 {
-	return sys_mprotect(sys->mem_base + args[0], args[1], args[2]);
+	int prot, prot_ok;
+
+	prot = f2b_prot(args[2], &prot_ok);
+	if (!prot_ok) {
+		debug("Unrecognised prot 0x%x\n", args[2]);
+		return -FRONT_EINVAL;
+	}
+
+	return sys_mprotect(sys->mem_base + args[0], args[1], prot);
 }
