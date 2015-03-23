3d03762b35de1eec1295085296fbc8033285eb83
Paul Burton <paul@archlinuxmips.org>
2015-03-23 18:38:37 +0000

    Add [gs]etpgrp syscalls
    Implement support for the getpgrp & setpgrp syscalls.
    
    Signed-off-by: Paul Burton <paul@archlinuxmips.org>

--- src/syscalls/pgrp.c
+++ src/syscalls/pgrp.c
@@ -1,6 +1,16 @@
 #include "syscall.h"
 
-uint32_t translate_sys_getpgrp(struct sys_state *sys, uint32_t *args)
+uint32_t translate_sys_getpgid(const struct sys_state *sys, uint32_t *args)
+{
+	return sys_getpgid(args[0]);
+}
+
+uint32_t translate_sys_setpgid(const struct sys_state *sys, uint32_t *args)
+{
+	return sys_setpgid(args[0], args[1]);
+}
+
+uint32_t translate_sys_getpgrp(const struct sys_state *sys, uint32_t *args)
 {
 	return sys_getpgrp();
 }
